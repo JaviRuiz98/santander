7b97f7d5e0fb361e1f515629e69dd52d6938592e
Javi Ruiz <franciscojavier.ruiz@tdconsulting.com>
2025-09-22 20:22:15 +0200

    front

--- frontend/src/app/pages/candidate-form/candidate-form.component.ts
+++ frontend/src/app/pages/candidate-form/candidate-form.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
 import { FormComponent } from '../../component/form/form.component';
 import { FormSchema } from '../../interface/component/form/form.interface';
 import { FORM_CANDIDATE_INPUTS } from '../../config/candidate-form/candidate-form.config';
 import { CandidateService } from '../../service/cadidate-form.service';
 import { IGenericResponse } from '../../interface/generic/IGenericResponse.interface';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-candidate-form',
-  imports: [FormComponent],
+  imports: [FormComponent, CommonModule],
   templateUrl: './candidate-form.component.html',
   styleUrl: './candidate-form.component.css',
   standalone: true,
 })
-
-export class CandidatePageComponent implements OnInit{
+export class CandidatePageComponent implements OnInit {
 
   formSchema: FormSchema[][] = FORM_CANDIDATE_INPUTS;
-  loading: boolean = false;
+  loading = false; 
 
-  constructor(private CandidateService: CandidateService){}
+  constructor(private CandidateService: CandidateService) {}
 
   ngOnInit(): void {}
 
-  onSubmit(event: any){
-    console.log('padre', event)
-    this.CandidateService.newCandidate(event).subscribe((response: IGenericResponse) => {
-
-    })
+  onSubmit(event: any) {
+    this.loading = true;
+    this.CandidateService.newCandidate(event).pipe(finalize(() => (this.loading = false))) .subscribe({
+      next: (response: IGenericResponse) => {
+        console.log('OK', response);
+      },
+      error: (err) => {
+        console.error('Error', err);
+      },
+    });
   }
 }
